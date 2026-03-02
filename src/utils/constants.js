c0a148f35d9048ddb9073c017ecb42a626c141b3
camelia0011 <cameliachowdhury12@gmail.com>
2026-03-02 14:11:59 +0530

    Replace hardcoded API key with environment variable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
-export const API_KEY = "dadae9b5528cc1f85c3ff74915fee3a5";
+export const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+console.log("KEY:", API_KEY);
